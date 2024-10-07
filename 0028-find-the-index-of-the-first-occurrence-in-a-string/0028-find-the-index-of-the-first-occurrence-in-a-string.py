d4be83c785b13a787e6aa56615008466f659fd3d
Haotian Wang <78605375+haotianw177@users.noreply.github.com>
2024-10-07 18:10:12 -0400

    Time: 12 ms (72.12%), Space: 11.4 MB (99.72%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -5,7 +5,8 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-
+        # we only need to check the lnegth of haystack till it exceed the possible contain length of needle
+        # if needle = ha, haystack = haha, only check until the third h in haystack cuz the rest of won't fit needle which is ha
         for i in range(len(haystack) - len(needle) + 1):
             # Check if the substring of haystack matches the needle
             if haystack[i:i+len(needle)] == needle:
