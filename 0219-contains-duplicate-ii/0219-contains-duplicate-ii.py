8d91360f58cce14bc5e3785a3e35fc99188c922b
Haotian Wang <78605375+haotianw177@users.noreply.github.com>
2024-10-08 14:18:50 -0400

    Time: 483 ms (52.58%), Space: 23.2 MB (26.15%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -6,10 +6,12 @@ class Solution(object):
         :rtype: bool
         """
 
+        # Find 2 numbers in the array that are equal and are at most k apart from each other."
+
         dict ={}
 
         for idx in range(len(nums)):
-            if nums[idx] in dict and abs(idx- dict[nums[idx]]) <=k:
+            if nums[idx] in dict and abs(idx - dict[nums[idx]]) <= k:
                 return True
             dict[nums[idx]] = idx
         return False
