6cb8fb0867da5a9397d598db4c03fd49f6f1b5ac
Haotian Wang <78605375+haotianw177@users.noreply.github.com>
2024-11-26 18:57:55 -0500

    Time: 0 ms (100%), Space: 12.4 MB (6.98%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        
+        hashMap ={}
+
+        for i in arr:
+            if i in hashMap:
+                hashMap[i] += 1
+            else:
+                hashMap[i] = 1
+        return len(hashMap.values()) == len(set(hashMap.values()))
+        
+
+        
